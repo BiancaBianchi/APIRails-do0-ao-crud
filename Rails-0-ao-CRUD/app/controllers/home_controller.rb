e0053ddfa78c5c44f7f035eceeb83719e5aa65a4
Bianca Bianchi <biabianchi7398@gmail.com>
2022-02-02 01:12:12 -0300

    #2 - Tasks funcionando no '/'

--- Rails-0-ao-CRUD/app/controllers/home_controller.rb
+++ Rails-0-ao-CRUD/app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController 
   def welcome
-    tasks = []
-    tasks << Task.new("Comprar pão")
-    tasks << Task.new("Comprar Leite")
+    @tasks = []
+    @tasks << Task.new("Comprar pão")
+    @tasks << Task.new("Comprar Leite")
   end
 end
